ddbee4c432fc217df208261063e2289ed9fd9b93
Bhagyashri Yogesh Patil <bhagyashriyogeshpatil@gmail.com>
2024-09-27 09:41:23 +0000

    Install Axios and configure axiosDefaults for API requests.

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://wanderhub-api-backend-8af792a9ebf9.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
