3fc25d465d00ad6ff93495d93e82f3d35408a928
Bhagyashri Yogesh Patil <bhagyashriyogeshpatil@gmail.com>
2024-09-27 15:26:05 +0000

    Update SignUpForm component with form structure and styles including username and password fields.

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -14,7 +14,26 @@ const SignUpForm = () => {
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>sign up</h1>
 
-            {/* add your form here */}
+          <Form>
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">username</Form.Label>
+              <Form.Control className={styles.Input} type="text" placeholder="Username" name="username" />
+            </Form.Group>
+
+            <Form.Group controlId="password1">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control className={styles.Input} type="password" placeholder="Password" name="password1" />
+            </Form.Group>
+
+            <Form.Group controlId="password2">
+              <Form.Label className="d-none">Confirm password</Form.Label>
+              <Form.Control className={styles.Input} type="password" placeholder="Confirm password" name="password2" />
+            </Form.Group>
+
+            <Button className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`} type="submit">
+              Sign Up
+            </Button>
+          </Form>
 
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
