9b4d595580b206e51ac38fb5919f6c00e32c04ba
Bhagyashri Yogesh Patil <bhagyashriyogeshpatil@gmail.com>
2024-09-27 21:50:25 +0000

    Add Axios interceptors to refresh access tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://wanderhub-api-backend-8af792a9ebf9.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
