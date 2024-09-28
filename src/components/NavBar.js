158be5b5159f4630f89582af6e56be17b2d494f6
Bhagyashri Yogesh Patil <bhagyashriyogeshpatil@gmail.com>
2024-09-28 14:36:38 +0000

    Add width attribute to logo image in NavBar for improved responsiveness on small screens

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -100,7 +100,7 @@ const NavBar = () => {
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
-            <img src={logo} alt="logo" height="75" />
+            <img src={logo} alt="logo" height="75" width="95"/>
           </Navbar.Brand>
         </NavLink>
         {currentUser && addPostIcon}
