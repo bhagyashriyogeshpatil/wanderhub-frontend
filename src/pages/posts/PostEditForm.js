fed8688676aa4b195d4b0bb0480c348754365e1f
Bhagyashri Yogesh Patil <bhagyashriyogeshpatil@gmail.com>
2024-09-29 16:05:27 +0000

    Add CommentCreateForm component so logged-in users can post comments on a post

--- src/pages/posts/PostEditForm.js
+++ src/pages/posts/PostEditForm.js
@@ -42,7 +42,7 @@ function PostEditForm() {
           ? setPostData({ title, content, place, region, image })
           : history.push("/");
       } catch (err) {
-        // console.log(err);
+        console.log(err);
       }
     };
 
@@ -83,7 +83,7 @@ function PostEditForm() {
       await axiosReq.put(`/posts/${id}/`, formData);
       history.push(`/posts/${id}`);
     } catch (err) {
-      // console.log(err);
+      console.log(err);
       if (err.response?.status !== 401) {
         setErrors(err.response?.data);
       }
