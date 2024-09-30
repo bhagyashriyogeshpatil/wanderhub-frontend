9e6aa144ecb9d32314f15a2cf090600b75f76b08
Bhagyashri Yogesh Patil <bhagyashriyogeshpatil@gmail.com>
2024-09-30 12:59:54 +0000

    Add functionality for users to edit username, password, and profile details

--- src/components/MoreDropdown.js
+++ src/components/MoreDropdown.js
@@ -2,6 +2,7 @@ import React from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 import styles from "../styles/MoreDropdown.module.css";
+import { useHistory } from "react-router";
 
 // The forwardRef is important!!
 // Dropdown needs access to the DOM node in order to position the Menu
@@ -34,7 +35,7 @@ export const MoreDropdown = ({ handleEdit, handleDelete }) => {
                         <i className="fas fa-edit" />
                     </Dropdown.Item>
                 </OverlayTrigger>
-                
+
                 <OverlayTrigger placement="top" overlay={<Tooltip>Delete</Tooltip>}>
                     <Dropdown.Item
                         className={styles.DropdownItem}
@@ -44,6 +45,37 @@ export const MoreDropdown = ({ handleEdit, handleDelete }) => {
                         <i className="fas fa-trash-alt" />
                     </Dropdown.Item>
                 </OverlayTrigger>
+            </Dropdown.Menu>
+        </Dropdown>
+    );
+};
+
+export function ProfileEditDropdown({ id }) {
+    const history = useHistory();
+    return (
+        <Dropdown className={`ml-auto px-3 ${styles.Absolute}`} drop="left">
+            <Dropdown.Toggle as={ThreeDots} />
+            <Dropdown.Menu>
+                <Dropdown.Item
+                    onClick={() => history.push(`/profiles/${id}/edit`)}
+                    aria-label="edit-profile"
+                >
+                    <i className="fas fa-edit" /> edit profile
+                </Dropdown.Item>
+                <Dropdown.Item
+                    onClick={() => history.push(`/profiles/${id}/edit/username`)}
+                    aria-label="edit-username"
+                >
+                    <i className="far fa-id-card" />
+                    change username
+                </Dropdown.Item>
+                <Dropdown.Item
+                    onClick={() => history.push(`/profiles/${id}/edit/password`)}
+                    aria-label="edit-password"
+                >
+                    <i className="fas fa-key" />
+                    change password
+                </Dropdown.Item>
             </Dropdown.Menu>
         </Dropdown>
     );
