9ac4453aa5d1c31005ca2183524f7cac6ba05ae2
Bhagyashri Yogesh Patil <bhagyashriyogeshpatil@gmail.com>
2024-09-30 20:02:03 +0000

    Setup project to create React unit tests

---
+++ src/mocks/handlers.js
@@ -0,0 +1,22 @@
+import { rest } from "msw";
+
+const baseURL = 'https://wanderhub-api-backend-8af792a9ebf9.herokuapp.com/';
+
+export const handlers = [
+    rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+        return res(
+          ctx.json({
+            pk: 3,
+            username: "Shree",
+            email: "",
+            first_name: "",
+            last_name: "",
+            profile_id: 3,
+            profile_image: "https://res.cloudinary.com/dssflljmx/image/upload/v1/media/images/lamb_1_oudtfv"
+            })
+        );
+    }),
+    rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
+        return res(ctx.status(200));
+    }),
+];
