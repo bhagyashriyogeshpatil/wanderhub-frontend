7d5b0d0abd48198d1efb8f7a5205419dc0a33e77
Bhagyashri Yogesh Patil <bhagyashriyogeshpatil@gmail.com>
2024-10-01 09:48:09 +0000

    Add tests for NavBar component rendering and user interaction

---
+++ src/components/__tests__/NavBar.test.js
@@ -0,0 +1,50 @@
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { CurrentUserProvider } from "../../contexts/CurrentUserContext";
+import NavBar from "../NavBar";
+
+test("renders NavBar", () => {
+  render(
+    <Router>
+      <NavBar />
+    </Router>
+  );
+
+  // screen.debug();
+  const signInLink = screen.getByRole("link", { name: "Sign in" });
+  expect(signInLink).toBeInTheDocument();
+});
+
+test("renders link to the user profile for a logged in user", async () => {
+  render(
+    <Router>
+      <CurrentUserProvider>
+        <NavBar />
+      </CurrentUserProvider>
+    </Router>
+  );
+
+  const profileAvatar = await screen.findByText("Shree");
+  expect(profileAvatar).toBeInTheDocument();
+});
+
+test("renders Sign in and Sign up buttons again on log out", async () => {
+  render(
+    <Router>
+      <CurrentUserProvider>
+        <NavBar />
+      </CurrentUserProvider>
+    </Router>
+  );
+
+  const signOutLink = await screen.findByRole("link", { name: "Sign out" });
+  fireEvent.click(signOutLink);
+
+  await waitFor(() => {   
+    const signInLink = screen.getByRole("link", { name: "Sign in" });
+    const signUpLink = screen.getByRole("link", { name: "Sign up" });
+  
+    expect(signInLink).toBeInTheDocument();
+    expect(signUpLink).toBeInTheDocument();
+  })
+});
